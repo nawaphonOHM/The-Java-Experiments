743afd1a1fed71c74b3bad7b534e26f6336b69f4
nawaphonOHM <nawaphon2539@gmail.com>
2019-09-30 00:32:29 +0700

    ....

---
+++ RegexTestHarness.java
@@ -0,0 +1,36 @@
+import java.io.Console;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegexTestHarness{
+    public static void main(String[] args){
+        Console console = System.console();
+        if(console == null){
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        while(true){
+            Pattern pattern = 
+                Pattern.compile(console.readLine("%nEnter your regex: "));
+            
+            Matcher mather = 
+                pattern.matcher(console.readLine("Enter input string to search: "));
+            
+            
+            boolean found = false;
+            while(mather.find()){
+                console.format("I found the text" + 
+                    " \"%s\" starting at " + 
+                    "index %d and ending at index %d.%n", 
+                    mather.group(), 
+                    mather.start(), 
+                    mather.end()
+                );
+                found = true;
+            }
+            if(!found){
+                console.format("No match found.%n");
+            }
+        }
+    }
+}
